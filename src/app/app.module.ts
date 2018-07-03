64dffd214ab817bf3d2144a73a4917a1dcaf606b
Dudix93 <dodo45693@gmail.com>
2018-07-03 21:18:34 +0200

    component pages & importing materialize-css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { SkicamsComponent } from './skicams/skicams.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SkicamsComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/skicams/skicams.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skicams',
+  templateUrl: './skicams.component.html',
+  styleUrls: ['./skicams.component.css']
+})
+export class SkicamsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
