c6a41d2266bac502259468e3ce8de3cc19a79cb8
Dudix93 <dodo45693@gmail.com>
2018-07-04 20:02:33 +0200

    getting name and cams

--- src/app/skicams/skicams.component.ts
+++ src/app/skicams/skicams.component.ts
@@ -8,8 +8,25 @@ import { RestapiServiceProvider } from '../../providers/restapi-service/restapi-
 })
 export class SkicamsComponent implements OnInit {
 
+  place: any = {name: '', cam: ''}
+  place2: any = {name: '', cam: ''}
+//'Vigo di Fassa', Piani di Bobbio
+  cams: Array<any> = []
   constructor(public api: RestapiServiceProvider) {
-    this.api.getCams().then(data => console.log(data));
+    this.api.getCams().then(data => {
+      for (const place in data) {
+        if (data[place].name === 'Vigo di Fassa') {
+          this.place.name = data[place].name;
+          for (let camera in data[place].cams) {
+            this.cams.push(data[place].cams[camera].url);
+            // if (data[i].cams[j].name === 'Vista sul centro sportivo') {
+            //   this.place.cam = data[i].cams[j].url;
+            // }
+          }
+          
+        }
+      }
+    });
    }
 
   ngOnInit() {
