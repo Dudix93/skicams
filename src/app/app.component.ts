d487b1c0c0773e7afd6bfbeaf3d712b42e22d486
Dudix93 <dodo45693@gmail.com>
2018-07-04 14:06:59 +0200

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './about/about.component.html',
-  styleUrls: ['./about/about.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterializeModule } from 'angular2-materialize';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { SkicamsComponent } from './skicams/skicams.component';
 import { ContactComponent } from './contact/contact.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'skicams', component: SkicamsComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +23,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    MaterializeModule
+    MaterializeModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
