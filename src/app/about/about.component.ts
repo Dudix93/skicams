f309ad5903c88e7e48343fe6996dce241cc1aec6
Dudix93 <dodo45693@gmail.com>
2018-07-06 13:22:07 +0200

    about us cards simplified addition

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AboutCard } from '../../models/aboutCard';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  aboutCardsRows: Array<any> = [];
+  aboutCards: Array<any> = [];
+  nextRow: Boolean = true;
+
   constructor() { }
 
   ngOnInit() {
+    this.newCard('img1.jpg', 'Lorem ipsum', 'Lorem ipsum dolor sit amet, ex nec legendos repudiandae, cu.');
+    this.newCard('img2.jpg', 'Lorem ipsum', 'Lorem ipsum dolor sit amet, ex nec legendos repudiandae, cu.');
+    this.newCard('img3.jpg', 'Lorem ipsum', 'Lorem ipsum dolor sit amet, ex nec legendos repudiandae, cu.');
   }
 
+  newCard(img: string, title: string, text: string) {
+    if (this.aboutCards.length === 0 || this.aboutCards.length % 3 === 0) {
+      this.aboutCards = [];
+      this.aboutCards.push(new AboutCard(img, title, text));
+      this.aboutCardsRows.push(this.aboutCards);
+    } else {
+      this.aboutCards.push(new AboutCard(img, title, text));
+      this.aboutCardsRows[this.aboutCardsRows.length - 1] = this.aboutCards;
+    }
+  }
 }
---
+++ src/models/aboutCard.ts
@@ -0,0 +1,8 @@
+export class AboutCard {
+
+    constructor(
+        public image_name: string,
+        public title: string,
+        public text: string
+    ) {}
+}
